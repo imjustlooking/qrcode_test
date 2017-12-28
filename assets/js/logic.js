9f04897db9061ceec070ffbd791c76e3c2ea6791
Gabriel <gabrielulu@gmail.com>
2017-12-28 17:47:13 +0800

    different default camera start depending on camera.length

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,36 +1,27 @@
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview') })
 scanner.addListener('scan', function (content) {
-  // console.log('found',content);
   document.getElementById('fill').value = content
 })
 Instascan.Camera.getCameras()
 .then(function (cameras) {
-  document.getElementById('fill').value = (cameras.length + ' camera(s)')
-  if (cameras.length > 0) {
-    console.log('cameras', cameras)
-    // need to toggle camera view between [0] to [1]
-    // let camSwitch = increment
+  // document.getElementById('fill').value = (cameras.length + ' camera(s)')
+  if (cameras.length === 1) {
+    scanner.start(cameras[0])
+  } else if (cameras.length > 1) {
     let index = 0
     document.getElementById('button1').addEventListener('click',
     function () {
-      // var abc = ['x','y']
-      if (cameras.length > 1) {
-        scanner.stop()
-        let increment = 1
-        if (index < cameras.length - 1) {
-          index += increment
-          scanner.start(cameras[index])
-        } else if (index === cameras.length - 1) {
-          index -= increment
-          scanner.start(cameras[index])
-        }
-        // index += increment
-        // console.log('index',index)
-      } else {
-        alert('You only have a single camera')
+      scanner.stop()
+      let increment = 1
+      if (index < cameras.length - 1) {
+        index += increment
+        scanner.start(cameras[index])
+      } else if (index === cameras.length - 1) {
+        index -= increment
+        scanner.start(cameras[index])
       }
     })
-    scanner.start(cameras[0])
+    scanner.start(cameras[1])
   } else {
     console.error('No cameras found.')
   }
