f303fa2f7af0ea5c4a7a51fe6d094a0f3f271089
Gabriel <gabrielulu@gmail.com>
2017-12-28 17:07:32 +0800

    Added button to toggle camera used

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,8 +9,25 @@ Instascan.Camera.getCameras()
     console.log('cameras', cameras)
     // need to toggle camera view between [0] to [1]
     // let camSwitch = increment
-    let increment = 0
-    // document.getElementById('button1')
+    document.getElementById('button1').addEventListener('click',
+    () => {
+      if (cameras.length > 1) {
+        scanner.stop()
+        let increment
+        let index = 0
+        if (index < cameras.length - 1) {
+          increment = 1
+        }
+        if (index === cameras.length - 1) {
+          increment = -1
+        }
+        index += increment
+        console.log(increment)
+        scanner.start(cameras[index])
+      } else {
+        alert('You only have a single camera')
+      }
+    })
     scanner.start(cameras[0])
   } else {
     console.error('No cameras found.')
