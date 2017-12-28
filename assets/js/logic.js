e28636924dfc45f86994b3365711f9fdc9b0ae7f
Gabriel <gabrielulu@gmail.com>
2017-12-28 22:13:43 +0800

    added copy, clear, visit button functionalities

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,7 +4,6 @@ scanner.addListener('scan', function (content) {
 })
 Instascan.Camera.getCameras()
 .then(function (cameras) {
-  // document.getElementById('fill').value = (cameras.length + ' camera(s)')
   if (cameras.length === 1) {
     scanner.start(cameras[0])
   } else if (cameras.length > 1) {
@@ -28,4 +27,23 @@ Instascan.Camera.getCameras()
 })
 .catch(function (e) {
   console.error(e)
+})
+
+document.getElementById('copy').addEventListener('click',
+function () {
+  document.getElementById('fill').select()
+  document.execCommand('copy')
+})
+document.getElementById('clear').addEventListener('click',
+function () {
+  document.getElementById('fill').value = ''
+})
+document.getElementById('visit').addEventListener('click',
+function () {
+  var inputCheck = document.getElementById('fill').value
+  if (inputCheck.slice(0,4) !== 'http'){
+    location.href = 'http://' + document.getElementById('fill').value
+  } else {
+    location.href = document.getElementById('fill').value
+  }
 })
