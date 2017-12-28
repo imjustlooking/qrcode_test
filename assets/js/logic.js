4b47a15aad9525c765eb6c2321f98a6787f6b96a
Gabriel <gabrielulu@gmail.com>
2017-12-28 16:18:54 +0800

    testing 2nd camera on mobile phone

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,18 @@
-let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') })
 scanner.addListener('scan', function (content) {
   // console.log('found',content);
   document.getElementById('fill').value = content
-});
-Instascan.Camera.getCameras().then(function (cameras) {
+})
+Instascan.Camera.getCameras()
+.then(function (cameras) {
   if (cameras.length > 0) {
-    scanner.start(cameras[0]);
+    console.log('cameras', cameras)
+    // let camSwitch = 
+    scanner.start(cameras[1])
   } else {
-    console.error('No cameras found.');
+    console.error('No cameras found.')
   }
-}).catch(function (e) {
-  console.error(e);
-});
+})
+.catch(function (e) {
+  console.error(e)
+})
